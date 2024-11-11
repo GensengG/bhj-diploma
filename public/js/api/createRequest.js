13834b210ffb99a1500960ae726e8f117b23da17
GensengG <168531024+GensengG@users.noreply.github.com>
2024-11-11 23:15:04 +0300

    Update createRequest.js

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -1,7 +1,30 @@
-/**
- * Основная функция для совершения запросов
- * на сервер.
- * */
 const createRequest = (options = {}) => {
+    const xhr = new XMLHttpRequest;
+    let url;
+    let data;
+    if (options.method) {
+        data = options.data;
+        url = options.url;
+        xhr.responseType = "json";
 
-};
+        if (options.method === "GET") {
+            url = url + "?"
+            for (value in options.data) {
+                url = url + value + "=" + options.data[value] + "&";
+            }
+            xhr.open(options.method, url);
+            xhr.send();
+        } else {
+            let formData = new FormData;
+            for (value in options.data) {
+                formData.append(value, options.data[value]);
+            }
+            xhr.open(options.method, url);
+	        xhr.send(formData);
+        }
+    }
+
+    xhr.addEventListener("load", () => {
+        options.callback(xhr.response.error, xhr.response);
+    })
+}
