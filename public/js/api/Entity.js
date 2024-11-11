4789a760347650f4a02027440da99e8885104eae
GensengG <168531024+GensengG@users.noreply.github.com>
2024-11-11 23:13:56 +0300

    Update Entity.js

--- public/js/api/Entity.js
+++ public/js/api/Entity.js
@@ -1,31 +1,27 @@
-/**
- * Класс Entity - базовый для взаимодействия с сервером.
- * Имеет свойство URL, равно пустой строке.
- * */
 class Entity {
-  /**
-   * Запрашивает с сервера список данных.
-   * Это могут быть счета или доходы/расходы
-   * (в зависимости от того, что наследуется от Entity)
-   * */
+  URL = "";
+
   static list(data, callback){
-
+    createRequest({
+      url: this.URL,
+      method: "GET",
+      });
   }
 
-  /**
-   * Создаёт счёт или доход/расход с помощью запроса
-   * на сервер. (в зависимости от того,
-   * что наследуется от Entity)
-   * */
+
   static create(data, callback) {
-
+    console.log(data);
+    createRequest({
+      url: this.URL,
+      method: "PUT",
+      });
   }
 
-  /**
-   * Удаляет информацию о счёте или доходе/расходе
-   * (в зависимости от того, что наследуется от Entity)
-   * */
   static remove(data, callback ) {
-
+    console.log(data);
+    createRequest({
+      url: this.URL,
+      method: "DELETE",
+      });
   }
 }
