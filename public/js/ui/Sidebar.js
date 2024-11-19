7a226d93935bfb5b92a7cc35c4d8c76eeb7e5c79
GensengG <168531024+GensengG@users.noreply.github.com>
2024-11-19 23:03:35 +0300

    Update Sidebar.js

--- public/js/ui/Sidebar.js
+++ public/js/ui/Sidebar.js
@@ -1,34 +1,43 @@
-/**
- * Класс Sidebar отвечает за работу боковой колонки:
- * кнопки скрытия/показа колонки в мобильной версии сайта
- * и за кнопки меню
- * */
 class Sidebar {
-  /**
-   * Запускает initAuthLinks и initToggleButton
-   * */
   static init() {
     this.initAuthLinks();
     this.initToggleButton();
   }
 
-  /**
-   * Отвечает за скрытие/показа боковой колонки:
-   * переключает два класса для body: sidebar-open и sidebar-collapse
-   * при нажатии на кнопку .sidebar-toggle
-   * */
   static initToggleButton() {
+    const sidebarBtn = document.querySelector(".sidebar-toggle");
+    const body = document.body;
 
+    sidebarBtn.addEventListener("click", () => {
+      body.classList.toggle("sidebar-open");
+      body.classList.toggle("sidebar-collapse");
+    })
   }
 
-  /**
-   * При нажатии на кнопку входа, показывает окно входа
-   * (через найденное в App.getModal)
-   * При нажатии на кнопку регастрации показывает окно регистрации
-   * При нажатии на кнопку выхода вызывает User.logout и по успешному
-   * выходу устанавливает App.setState( 'init' )
-   * */
   static initAuthLinks() {
+    const modalRegister = document.querySelector("#modal-register");
+    const regist = document.querySelector(".menu-item_register");
+    regist.addEventListener("click", (e) => {
+      e.preventDefault();
+      const modal = new Modal(modalRegister);
+      modal.open();
+    })
 
+    const modalLogin = document.querySelector("#modal-login");
+    const log = document.querySelector(".menu-item_login");
+    log.addEventListener("click", (e) => {
+      e.preventDefault();
+      const modal = new Modal(modalLogin);
+      modal.open();
+    })
+
+    const modalLogout = document.querySelector(".menu-item_logout");
+    modalLogout.addEventListener("click", (e) => {
+      e.preventDefault();
+      User.logout();
+      if(response.success){
+        App.setState("init");
+      }
+    })
   }
 }
